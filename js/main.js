9bb4c49d3771422a9d5ce584a14a6f28eb662590
daniele-pipino <Danielemacpipino@icloud.com>
2021-06-30 15:19:19 +0200

    commit iniziale + console log

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// console log
+console.log('js ok');
+
+/*
+Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+ */
+
